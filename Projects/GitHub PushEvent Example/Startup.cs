2688390ce4124bb09ba3159e502a9f7ba16e40e2
Tim Murphy <tim@26tp.com>
2014-04-28 05:44:40 +1000

    Added "GitHub PushEvent Example" website project.

---
+++ Projects/GitHub PushEvent Example/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(GitHub.PushEvent.Example.Startup))]
+
+namespace GitHub.PushEvent.Example
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}
